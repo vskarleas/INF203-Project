0abd8261ce5a35cd14ba641a329dbdfa04796d85
vskarleas <vskarleas@icloud.com>
2022-03-04 13:17:08 +0100

    rapport updated

--- changelog.md
+++ changelog.md
@@ -1,3 +1,13 @@
+**Version 1.5.4**
+
 * cow_university has a problem
 * crazy_cow is not completed. It needs to add the questions and add smart functionality.
 * smart_cow works if it identifies correctly the operators. Otherwise, we could create seperate smart_cow files for the specific operation.
+
+---
+
+**Version 1.5.6**
+
+* cow_university has a problem
+* crazy_cow is not completed. It needs to add the questions and add smart functionality.
+* smart_cow has been fixed. There is one error with the multiplication function right now.
--- smart_cow.sh
+++ smart_cow.sh
@@ -1,30 +1,31 @@
-if [ "$1" == *"+"* ]
+echo $1
+kappa=$(echo $1 | cut -d" " -f 2)
+if [ "$kappa" == "+" ]
 then
 echo +
 n1=$(echo $1 | cut -d+ -f 1)
 n2=$(echo $1 | cut -d+ -f 2)
 lamda=$(expr $n1 + $n2)
-cowsay $1=$lamda
-elif [ "$1" == *"-"* ]
+cowsay "$1 = $lamda"
+elif [ "$kappa" == "-" ]
 then
 echo -
 n1=$(echo $1 | cut -d- -f 1)
 n2=$(echo $1 | cut -d- -f 2)
 lamda=$(expr $n1 - $n2)
 cowsay "$n1-$n2=$lamda"
-elif [ "$1" == *"/"* ]
+elif [ "$kappa" == "/" ]
 then
 echo /
 n1=$(echo $1 | cut -d/ -f 1)
 n2=$(echo $1 | cut -d/ -f 2)
 lamda=$(expr $n1 / $n2)
-cowsay $1=$lamda
+cowsay "$1 = $lamda"
 else
-echo \*
 n1=$(echo $1 | cut -d* -f 1)
 n2=$(echo $1 | cut -d* -f 2)
 echo $n1
 echo $n2
 lamda=$(( $n1 * $n2))
-cowsay "$n1\*$n2=$lamda"
+cowsay "$n1 \* $n2 = $lamda"
 fi
